c3d40bff505d87883fe98be6912c2edbf999078a
cells <open-source-version@proton.me>
2025-09-06 19:50:34 +0300

    Commit for cloning

---
+++ encryptor.py
@@ -0,0 +1,24 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    fileName = input("Which file do you want to encrypt?: ")
+
+    files.append(fileName)
+    break
+
+print(files)
+
+key = Fernet.generate_key()
+
+with open("decryption_key.txt", "wb") as decryption_key:
+    decryption_key.write(key)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
